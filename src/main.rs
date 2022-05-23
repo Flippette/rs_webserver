bb6c4ffb68d0b7f3c3195b920789e035b975ee1b
Flippette <quangdatle2006@outlook.com>
2022-05-23 23:02:26 +0700

    Enabled customizable addresses via argv

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use rs_webserver::*;
 use std::net::TcpListener;
 use std::sync::Arc;
 use std::thread;
+use std::env;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").expect("Failed to bind to localhost:7878!");
+    let addr = match env::args().nth(1) {
+        Some(arg) => arg,
+        None => "127.0.0.1:7878".to_string()
+    };
+    println!("Starting server at {}!", &addr);
+    let listener = TcpListener::bind(&addr).expect(format!("Failed to bind to {}!", addr).as_str());
     let pool = ThreadPool::new(thread::available_parallelism().unwrap().get())
         .expect("Failed to create thread pool!");
 
